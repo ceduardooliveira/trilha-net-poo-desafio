a44887697d7e899371d8f0f31c9dd5d5363885e2
ceduardooliveira <c-eduardo94@hotmail.com>
2024-09-12 20:14:36 -0300

    ✨ feat: Desáfio Concluído!!!

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,20 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        // Propriedades implementadas
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
+            // parâmetros implementados            
         }
 
         public void Ligar()
